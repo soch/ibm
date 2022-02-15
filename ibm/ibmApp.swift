972de6a32aef58cf7b2534a64f0d161cf93a7dd5
Amit Jain <amitjain16@gmail.com>
2022-02-14 23:42:51 -0800

    Initial Commit

---
+++ ibm/ibmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibmApp.swift
+//  ibm
+//
+//  Created by Amit Jain on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ibmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
